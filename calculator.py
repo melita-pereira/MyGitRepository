9ae13346d4ebb61a8764ef7e6fbc208b1ccb9bda
Kayla2519 <kayla2519@icloud.com>
2026-02-10 15:04:59 -0400

    Add calculator file

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Calculator #
+
+def addition(a, b):
+    return a+b
+
+def subtraction(a, b):
+    return a-b
+
+def multiplication(a, b):
+    return a*b
+
+def division(a, b):
+    if (b == 0):
+        return "Error: Cannot divide by zero"
+    return a/b
+
